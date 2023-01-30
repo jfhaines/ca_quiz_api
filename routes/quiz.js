0b51a53094b22e52025fbdacee71af7e78c2437a
2w00fs <63689305+2w00fs@users.noreply.github.com>
2023-01-31 08:33:47 +1000

    GET quiz implemented

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,54 +1,96 @@
 import express from 'express'
-import { FlashcardModel, QuizModel } from '../db.js'
+import {FlashcardModel, QuizModel, SubjectModel} from '../db.js'
 
 const router = express.Router()
 
+// checks card values match type and are not empty
+// need to test if it works, if returns undefined need to make it a Promise
+function parseCards({answerOptions, question}) {
+    if (typeof question === "string" &&  answerOptions.constructor === Array) {
+        return answerOptions.every(({isCorrectOption, text}) => {
+            return typeof text === "string" && text.length > 0 && typeof isCorrectOption === "boolean";
+        })
+    }
+}
+// List the available quizzes, name used for display, _id used for selection
 router.get('/', async (req, res) => {
-    //res.send(await QuizModel.findById("63d1de4bb612e7efe6b2ef2c").populate({path: 'question', select: "question"} ))
-    res.send(await QuizModel.find().populate({path: 'question'} ))
+    res.send(await QuizModel.find().select({_id: 1, name: 1}))
+})
+
+// Select card by _id to return flashcards
+router.get('/cards', async (req, res) => {
+    const id = await req.query.id
+    const cardData = await QuizModel.findById(id)
+    res.send(cardData)
 })
 
 
-router.get('/cards/add/all', async (req, res) => {
-    const cards = await FlashcardModel.find()
-    const cardIds = Array.from(cards,v=>String(v._id))
-    console.log(cardIds)
-    const q = await QuizModel.findById("63d1d71cbb04d26dc1590579")
-    q.question = cardIds
-    q.save()
-    res.send(await QuizModel.find().populate({path: 'question'} ))
-
+// Create quiz
+/*
+summary: "Create a new quiz",
+description: "",
+requestSchema:
+    params:
+        subjectId:
+            description: "Subject ID",
+            required: true
+        quizName:
+            description: "New name for the quiz",
+            required: true
+        flashcard:
+            description: "Object containing flashcard values",
+            required: false
+ */
+router.post('/new', async (req, res) => {
+    const { subjectId, quizName, flashcard } = req.body
+    const newQuiz = { name: quizName, subjectID: subjectId, flashcards: flashcard }
+    const insertQuiz = await QuizModel.create(newQuiz)
+    res.status(201).send(await insertQuiz)
 })
 
-router.get('/:id', async (req, res) => {
-    try {
-        const entry = await QuizModel.findById(req.params.name).populate({ path: 'subject', select: 'name' })
-        if (entry) {
-            res.send(entry)
-        } else {
-            res.status(404).send({ error: 'Entry not found' })
-        }
-    }
-    catch (err) {
-        res.status(500).send({ error: err.message })
-    }
-})
+// Update quiz with new card/s
+/*
+summary: "Update a quiz",
+description: "Updates quiz with one or many flashcards, quiz name can also be changed",
+requestSchema:
+    params:
+        subjectId:
+            description: "Subject ID",
+            required: true
+        quizName:
+            description: "New name for the quiz",
+            required: true
+        flashcard:
+            description: "Object containing flashcard values", required: false,
+            innerSchema:
+                params:
+                    question: String, required: true,
+                    answerOptions: [{}], required: true
+                        innerSchema:
+                            text: String, required: true,
+                            isCorrectOption: Boolean, required: true
+ */
+router.put('/update', async (req, res) => {
+    const { subjectId, quizId, quizName, flashcard } = req.body
 
-// Update
-router.put('/:id', async (req, res) => {
-    const { subject, content } = req.body
-    const newEntry = { subject, content }
 
-    try {
-        const entry = await QuizModel.findByIdAndUpdate(req.params.id, newEntry, { returnDocument: 'after' })
-        if (entry) {
-            res.send(entry)
-        } else {
-            res.status(404).send({ error: 'Entry not found' })
-        }
-    }
-    catch (err) {
-        res.status(500).send({ error: err.message })
+    QuizModel.findById( quizId, quiz => {
+        quiz.flashcards.push(flashcard)
+        quiz.save()
+    })
+    const update = await SubjectModel.findByIdAndUpdate(quizId,
+        {
+            name: quizName,
+            subjectID: subjectId,
+            flashcards: flashcards
+        },
+        { returnDocument: 'after' }
+    )
+
+    if (update) {
+        res.send(update)
+    } else {
+        res.status(404).send({ error: 'Entry not found' })
     }
 })
 
@@ -61,23 +103,6 @@ router.delete('/:id', async (req, res) => {
         } else {
             res.status(404).send({ error: 'Entry not found' })
         }
-    }
-    catch (err) {
-        res.status(500).send({ error: err.message })
-    }
-})
-
-router.post('/', async (req, res) => {
-    try {
-        // 1. Create a new entry object with values passed in from the request
-        const { subjects, content } = req.body
-        const subjectObject = await QuizModel.findOne({ name: subjects })
-        const newEntry = { name: subjectObject.name, content }
-        // 2. Push the new entry to the entries array
-        // entries.push(newEntry)
-        const insertedEntry = await QuizModel.create(newEntry)
-        // 3. Send the new entry with 201 status
-        res.status(201).send(await insertedEntry.populate({ path: 'subject', select: 'name' }))
     }
     catch (err) {
         res.status(500).send({ error: err.message })
